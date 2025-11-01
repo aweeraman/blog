a5d6925a178526f841eb3c84e96fd28e5c2d59e0
Anuradha Weeraman <anuradha@weeraman.com>
2025-11-01 21:02:01 +0530

    feat: Google analytics integration

--- pages/+Head.tsx
+++ pages/+Head.tsx
@@ -4,6 +4,19 @@ export function Head() {
     <>
       <meta charSet="UTF-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+      {/* Google Analytics */}
+      <script async src="https://www.googletagmanager.com/gtag/js?id=G-T3167J0GT8"></script>
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-T3167J0GT8');
+          `,
+        }}
+      />
     </>
   );
 }
